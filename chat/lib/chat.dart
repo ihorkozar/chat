744d6f3888317dbc2d5639b52518397a36649ee5
Ihor Kozar <iak@anahorets-iMac.local>
2022-06-30 14:54:42 +0300

    Initial commit

---
+++ chat/lib/chat.dart
@@ -0,0 +1,5 @@
+library chat;
+
+class Calculator {
+
+}
---
+++ chat/lib/src/services/user_service_contract_impl.dart
@@ -0,0 +1,52 @@
+import 'package:chat/src/models/user.dart';
+import 'package:chat/src/services/user_service_contract.dart';
+import 'package:rethink_db_ns/rethink_db_ns.dart';
+
+class UserService implements IUserService {
+  final Connection connection;
+  final RethinkDb rethinkdb;
+
+  UserService(this.rethinkdb, this.connection);
+
+  @override
+  Future<User> connect(User user) async {
+    var data = user.toJson();
+    if (user.id != null) {
+      data['id'] = user.id;
+    }
+
+    final result = await rethinkdb.table('users').insert(
+        data, {'conflict': 'update', 'return_changes': true}).run(connection);
+
+    return User.fromJson(result['changes'].first['new_val']);
+  }
+
+  @override
+  Future<void> disconnect(User user) async {
+    await rethinkdb.table('users').update({
+      'id': user.id,
+      'is_active': false,
+      'last_seen': DateTime.now()
+    }).run(connection);
+
+    connection.close();
+  }
+
+  @override
+  Future<User?> fetch(String id) async {
+    final user = await rethinkdb.table('users').get(id).run(connection);
+    if (user != null) {
+      return User.fromJson(user);
+    }
+  }
+
+  @override
+  Future<List<User>> getOnlineUsers() async {
+    Cursor users = await rethinkdb
+        .table('users')
+        .filter({'is_active': true}).run(connection);
+
+    final usersList = await users.toList();
+    return usersList.map((item) => User.fromJson(item)).toList();
+  }
+}
